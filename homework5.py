11b99b539ad0b56148f122180f1e519225590828
Alehxh <pen3008kow@gmail.com>
2024-06-14 20:41:33 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, "Melon", True, 2, 4
+print (immutable_var)
+immutable_var [0] = "apple"  #Ошибка при print - TypeError: 'tuple' object does not support item assignment
+print (immutable_var)       #Не получилось изменить т.к тип tup является неизменяемым типом данных
+mutable_list = [0, 1 ,'apple ', True, 7, 92]
+print (mutable_list)
+mutable_list[0] = 22
+mutable_list [2] = 'two'
+print (mutable_list)
