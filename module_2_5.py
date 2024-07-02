e62394f8007f499474ba2cd594d69936db59f5a5
Alehxh <pen3008kow@gmail.com>
2024-07-02 03:33:17 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,9 @@
+def get_matrix (n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append(n)
+        for j in range(m):
+            matrix.append(value)
+    print(matrix)
+get_matrix(1, 1, 10)
+
